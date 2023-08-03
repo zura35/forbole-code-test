e5b3742f53b61a989b4619541582be83fe3ea0be
Jenny Leung <zura35@gmail.com>
2023-08-04 01:34:27 +0800

    fix: db connection issue with app run on docker

--- cmd/user_etl/user_etl.go
+++ cmd/user_etl/user_etl.go
@@ -18,8 +18,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var (
+	dbHost     = os.Getenv("DB_HOST")
+	dbPort     = os.Getenv("DB_PORT")
+	dbUser     = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbName     = os.Getenv("DB_NAME")
+)
+
 func main() {
-	dsn := "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 	db, err := sql.Open("postgres", dsn)
 	defer db.Close()
 
@@ -46,8 +55,14 @@ func main() {
 			time.Sleep(interval)
 
 			ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-			defer cancel()
-			go userService.FetchAndCreateUser(ctx)
+			go func() {
+				defer cancel()
+
+				err := userService.FetchAndCreateUser(ctx)
+				if err != nil {
+					fmt.Printf("error: %v\n", err)
+				}
+			}()
 		}
 	}
 }
