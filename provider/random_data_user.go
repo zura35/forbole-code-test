39729b6703f185281b298ec6ab2a82aa9539e6f8
Jenny Leung <zura35@gmail.com>
2023-08-03 19:00:47 +0800

    feat: get user data from random data api and parse to user model

---
+++ provider/random_data_user.go
@@ -0,0 +1,71 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"forbole_code_test/model"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RandomDataUser struct{}
+
+type userJSON struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	DOB       string `json:"date_of_birth"`
+	Address   struct {
+		City          string `json:"city"`
+		StreetName    string `json:"street_name"`
+		StreetAddress string `json:"street_address"`
+		ZipCode       string `json:"zip_code"`
+		State         string `json:"state"`
+		Country       string `json:"country"`
+		Coordinates   struct {
+			Latitude  float32 `json:"lat"`
+			Longitude float32 `json:"lng"`
+		}
+	} `json:"address"`
+}
+
+const (
+	url = "https://random-data-api.com/api/users/random_user"
+)
+
+func NewRandomDataUser() *RandomDataUser {
+	return &RandomDataUser{}
+}
+
+func (r *RandomDataUser) GetRandomUser(ctx context.Context) (*model.User, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var uJSON userJSON
+	err = json.Unmarshal(body, &uJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	dob, err := time.Parse(time.DateOnly, uJSON.DOB)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.User{
+		FirstName: uJSON.FirstName,
+		LastName:  uJSON.LastName,
+		DOB:       dob,
+		// Address:   uJSON.Address,
+	}, nil
+
+}
---
+++ provider/random_data_user_test.go
@@ -0,0 +1,24 @@
+package provider_test
+
+import (
+	"context"
+	"forbole_code_test/provider"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetRandomUser(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	userProvider := provider.NewRandomDataUser()
+	user, err := userProvider.GetRandomUser(ctx)
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, user.FirstName)
+	assert.NotEmpty(t, user.LastName)
+	assert.Equal(t, user.DOB.IsZero(), false)
+	assert.NotEmpty(t, user.Address)
+}
